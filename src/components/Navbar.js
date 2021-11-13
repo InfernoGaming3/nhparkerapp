88ed4adcbeb966c7daee19f780568598d9d04bc8
CBColleg3 <cbcolleg@udel.edu>
2021-11-13 00:54:46 -0500

    save point

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,38 @@
 import React, {useState} from 'react';
+import './Navbar.css';
 import { Link } from 'react-router-dom';
+import { Button } from './Button';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const[button, setButton] = useState(true);
+
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    };
+
+    window.addEventListener('resize', showButton);
+
+
+
     return (
         <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to='/' className='navbar-logo'>
                         TRVL <i className='fab fa-typo3' />
                     </Link>
                     <div classNames='menu-icon' onClick = {handleClick}></div>
                         <i classNames={click ? 'fas fa-times' : 'fas fa-bars'} />
                 </div>
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}></ul>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
 
                 <li className='nav-item'>
                     <Link to='/' className='nav-links' onClick={closeMobileMenu}>
@@ -36,7 +53,9 @@ function Navbar() {
                     <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
                         Sign-Up
                     </Link>
-                </li>
+                    </li>
+                </ul>
+                {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
             </nav>
         </>
     )
